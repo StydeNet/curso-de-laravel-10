d19aae767cd565627cb4770e2d6afcae7dff716a
Duilio Palacios <duilio@styde.net>
2023-12-10 08:57:02 +0000

    Add validation rules

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Note;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rule;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +38,11 @@ Route::get('/notas/crear', function () {
 })->name('notes.create');
 
 Route::post('/notas', function (Request $request) {
+    $request->validate([
+        'title' => ['required', 'min:3', Rule::unique('notes')],
+        'content' => 'required',
+    ]);
+
     Note::create([
         'title' => $request->input('title'),
         'content' => $request->input('content'),
